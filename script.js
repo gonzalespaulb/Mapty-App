1e2d0251c69e0ea20aa4b68d5e28511f926ca8dc
Paul Gonzales <paulgonzales@Pauls-Mac-mini.local>
2021-01-18 13:40:06 -0800

    input fields

--- script.js
+++ script.js
@@ -11,14 +11,60 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if(navigator.geolocation)
 navigator.geolocation.getCurrentPosition(function(position){
     const {latitude} = position.coords;
     const {longitude} = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`); 
-}, function(){
+
+    const coords = [latitude, longitude];
+
+    map = L.map('map').setView(coords, 13);
+    // console.log(map);
+
+    // Youre able to change the theme by messing with the .fr/hot
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Handling clicks on map
+    map.on(`click`, function(mapE){
+        mapEvent = mapE;
+        form.classList.remove(`hidden`);
+        inputDistance.focus();
+    });
+}, 
+    function(){
     alert(`Could not get your position`)
 })
 
+form.addEventListener(`submit`, function(e){
+e.preventDefault();
 
-  
+    // Clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ``;
+
+    // Displays the marker 
+        console.log(mapEvent);
+        const{lat, lng} = mapEvent.latlng;
+
+        L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidght: 100, 
+            autoClose: false,
+            closeOnClick: false,
+            className: `running-popup`,
+        }))
+        .setPopupContent(`Workout`) 
+        .openPopup();
+})
+
+// Changes the elevation gain and duration when toggled
+inputType.addEventListener(`change`, function(){
+    inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`)
+    inputCadence.closest(`.form__row`).classList.toggle(`form__row--hidden`)
+})
